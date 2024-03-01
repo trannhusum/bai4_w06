87aadfd03a6fee8fbf8747e576e304dc32bbc5ec
trannhusum <147652638+trannhusum@users.noreply.github.com>
2024-03-01 15:04:35 +0700

    Initial commit

---
+++ Fan.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+namespace bai4_w06
+{
+    public class Fan
+    {
+        public const int slow = 1;
+        public const int medium = 2;
+        public const int fast = 3;
+        private int speed {  get; set; }
+        private bool on {  get; set; }
+        private double radius { get; set; }
+        private string color { get; set; }
+
+        public Fan()
+        {
+            speed = slow;
+            on=false;
+            radius = 5;
+            color = "blue";
+        }
+
+        public Fan(int speed, bool on, double radius, string color)
+        {
+            this.speed = speed;
+            this.on = on;
+            this.radius = radius;
+            this.color = color;
+        }
+
+        public string ToString()
+        {
+            if (on == true)
+                return "Speed: " + speed+", Color: "+color+", Radius: "+radius+", fan is on";
+            else
+                return "Color: " + color + ", Radius: " + radius + ", fan is off";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai4_w06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fan quat1 = new Fan(Fan.fast, true, 10, "yellow");
+            Fan quat2 = new Fan(Fan.medium, false, 5, "blue");
+            Console.WriteLine(quat1.ToString());
+            Console.WriteLine(quat2.ToString());
+            Console.ReadKey();
+        }
+    }
+}
